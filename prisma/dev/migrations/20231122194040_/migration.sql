f90a96f9f1085e9971eedb8daaad5438e5d58a8e
katzeallergie <tatsuhiko_saito@r.recruit.co.jp>
2023-11-23 04:41:17 +0900

    fixxxxxx

---
+++ prisma/dev/migrations/20231122194040_/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "memos" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "memos_pkey" PRIMARY KEY ("id")
+);
